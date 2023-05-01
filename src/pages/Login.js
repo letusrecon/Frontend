ab62b01266afda709858b2c5ee6e1c75750cfcbd
Aytech <ayokunumitoluwani00@gmail.com>
2023-05-01 02:20:57 +0100

    regsiter and login done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,59 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './Login.module.css'
+
 export default function Login() {
   return (
-    <div>
-      <h1>Hello Login</h1>
+    <div className={styles.main_login_container}>
+      <div className={styles.login_form_col}>
+        <form className={styles.login_form} action="">
+          <h1 className={styles.login_heading}> LetUsRecon Login</h1>
+        
+
+          <div className={styles.login_input_wrap}>
+            <label className={styles.login_label} htmlFor="email">
+              Email
+            </label>
+            <input name="email" className={styles.login_input} type="text" />
+          </div>
+
+          <div className={styles.login_input_wrap}>
+            <label className={styles.login_label} htmlFor="password">
+              Password
+            </label>
+            <input
+              name="password"
+              className={styles.login_input}
+              type="password"
+            />
+          </div>
+
+
+          <div className={styles.login_help_links_wrap}>
+            <p className={styles.login_text}>
+              Forgot Password ?{" "}
+              <Link className={styles.login_links} href="/">
+                Click here
+              </Link>{" "}
+             
+            </p>
+
+            <div className={styles.login_action_btn_wrap}>
+              <button className={styles.login_action_btn}>Submit</button>
+            </div>
+            <p className={styles.login_text}>
+              You don't have an account ?{" "}
+              <Link className={styles.login_links} href="/Register">
+                Create an account here
+              </Link>{" "}
+            </p>
+          </div>
+        </form>
+      </div>
+
+      <div className={styles.login_img_col}>
+        <Image src="/assets/hack01.jpg" width="930" height="1000" />
+      </div>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,86 @@
+
+import Image from "next/image";
+import styles from './Register.module.css'
+import Link from "next/link";
+
 export default function Register (){
-    return(
-        <div>
-            <h1>Register</h1>
+    return (
+      <div className={styles.main_register_container}>
+        <div className={styles.form_col}>
+          <form className={styles.register_form} action="">
+            <h1 className={styles.register_heading}> Registration</h1>
+            <div className={styles.register_input_wrap}>
+              <label className={styles.register_label} htmlFor="name">
+                Name
+              </label>
+              <input
+                name="name"
+                className={styles.register_input}
+                type="text"
+              />
+            </div>
+
+            <div className={styles.register_input_wrap}>
+              <label className={styles.register_label} htmlFor="email">
+                Email
+              </label>
+              <input
+                name="email"
+                className={styles.register_input}
+                type="text"
+              />
+            </div>
+
+            <div className={styles.register_input_wrap}>
+              <label className={styles.register_label} htmlFor="password">
+                Password
+              </label>
+              <input
+                name="password"
+                className={styles.register_input}
+                type="password"
+              />
+            </div>
+
+            <div className={styles.register_input_wrap}>
+              <label className={styles.register_label} htmlFor="password">
+                Confirm Password
+              </label>
+              <input
+                name="password"
+                className={styles.register_input}
+                type="password"
+              />
+            </div>
+
+            <div className={styles.help_links_wrap}>
+              <p className={styles.register_text}>
+                By signing up, you agree to our{" "}
+                <Link className={styles.register_links} href="/Terms">
+                Terms of Service
+                </Link>{" "}
+                and{" "}
+                <Link className={styles.register_links} href="/Policy">
+                  Privacy Policy
+                </Link>
+              </p>
+
+              <div className={styles.register_action_btn_wrap}>
+                <button className={styles.register_action_btn}>Submit</button>
+              </div>
+              <p className={styles.register_login_text}>
+                Already own an account ?{" "}
+                <Link className={styles.register_links} href="/Login">
+                  Login
+                </Link>{" "}
+              </p>
+            </div>
+          </form>
         </div>
-    )
+
+        <div className={styles.img_col}>
+          <Image src="/assets/hack02.jpg" width="930" height="1000" />
+        </div>
+      </div>
+    );
 }
