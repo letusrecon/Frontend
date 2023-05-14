6f614dbd122df78a6c517f29dcd0ff1f6da510fe
Aytech <ayokunumitoluwani00@gmail.com>
2023-05-14 23:58:44 +0100

    form data init

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -21,7 +21,88 @@ export const TESTIMONIAL_DATA = [
 ];
 
 
-
+export const SCAN_HISTORY_DATA = [
+  {
+    id: 1,
+    "Target name": "tamu.edu",
+    "Scan type": "mayoclinic.com",
+    "Last scanned": "5/5/2023",
+    Status: "lcornock0",
+    Progress: "1461",
+    Result: "Basic Industries",
+    Action: "n/a",
+  },
+  {
+    id: 2,
+    "Target name": "opensource.org",
+    "Scan type": "blinklist.com",
+    "Last scanned": "2/18/2023",
+    Status: "dmadgett1",
+    Progress: "61",
+    Result: "Transportation",
+    Action: "Energy",
+  },
+  {
+    id: 3,
+    "Target name": "moonfruit.com",
+    "Scan type": "tamu.edu",
+    "Last scanned": "9/19/2022",
+    Status: "bfields2",
+    Progress: "111",
+    Result: "Finance",
+    Action: "Consumer Services",
+  },
+  {
+    id: 4,
+    "Target name": "plala.or.jp",
+    "Scan type": "earthlink.net",
+    "Last scanned": "6/26/2022",
+    Status: "tadamthwaite3",
+    Progress: "900",
+    Result: "Capital Goods",
+    Action: "Technology",
+  },
+  {
+    id: 5,
+    "Target name": "soup.io",
+    "Scan type": "xing.com",
+    "Last scanned": "7/12/2022",
+    Status: "dpaladino4",
+    Progress: "350",
+    Result: "Technology",
+    Action: "Consumer Services",
+  },
+  {
+    id: 6,
+    "Target name": "t.co",
+    "Scan type": "vinaora.com",
+    "Last scanned": "5/16/2022",
+    Status: "ainderwick5",
+    Progress: "774",
+    Result: "Finance",
+    Action: "Health Care",
+  },
+  {
+    id: 7,
+    "Target name": "cnet.com",
+    "Scan type": "wisc.edu",
+    "Last scanned": "5/3/2023",
+    Status: "llittleton6",
+    Progress: "125",
+    Result: "Consumer Services",
+    Action: "Finance",
+  },
+  {
+    id: 8,
+    "Target name": "jiathis.com",
+    "Scan type": "ebay.com",
+    "Last scanned": "7/28/2022",
+    Status: "lmacpeice7",
+    Progress: "81",
+    Result: "Health Care",
+    Action: "Health Care",
+  },
+];
 
 
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,66 +1,111 @@
-import Layout from "@/Components/Layout"
+import Layout from "@/Components/Layout";
 import Link from "next/link";
-import {CgProfile} from "react-icons/cg"
-import styles from "./Dasboard.module.css"
+import { CgProfile } from "react-icons/cg";
+import styles from "./Dasboard.module.css";
+import DataTable from "react-data-table-component";
+import { SCAN_HISTORY_DATA } from "@/Data/Data";
+import { MdArrowDropDown } from "react-icons/md";
+import { AiOutlineSearch } from "react-icons/ai";
 
-
-export default function Dashboard (){
-    return (
-      <Layout>
-        <div className={styles.dashboard_container}>
-          <div className=" flex justify-between items-center">
-            <div className="flex items-center">
-              <CgProfile className=" text-7xl text-slate-100 mr-3" />
-              <div className="">
-                <h2 className="text-slate-100">Hell Ismail,</h2>
-                <h2 className="text-slate-100">Welcome Back</h2>
-              </div>
-            </div>
-
-            <div className="flex ">
-
-              <form action="" className="mr-8 mt-3">
-                <input type="text" placeholder="Example.com" className="rounded-md border py-1 px-6 w-4/3 bg-transparent " />
-              </form>
-              <div>
-                <CgProfile className="text-2xl mx-auto text-slate-100"/>
-                <Link className="text-slate-100" href="/"> My profile</Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="mt-14">
-            <h3 className="text-slate-100 text-3xl">Today</h3>
-            <div className="flex mt-10">
-              <div className="border bg-[#141C1D] py-4 px-10 rounded-xl mr-7">
-                <h3 className="text-slate-100 ">Scan Performed</h3>
-                <h1 className="text-center text-white mt-3 text-5xl">0</h1>
-              </div>
-              <div className="border bg-[#141C1D] py-4 px-10  rounded-xl mr-7">
-                <h3 className="text-slate-100">Running scans</h3>
-                <h1 className="text-center text-white mt-3 text-5xl">0</h1>
-              </div>
-              <div className="border bg-[#141C1D] py-4 px-20 rounded-xl mr-7">
-                <h3 className="text-slate-100">Target</h3>
-                <h1 className="text-center text-white mt-3 text-5xl">0</h1>
-              </div>
-              <div className="border bg-[#141C1D] py-4 px-10  rounded-xl p-5">
-                <h3 className="text-slate-100">Subdomains Found</h3>
-                <h1 className="text-center text-white mt-3 text-5xl">0</h1>
-              </div>
+export default function Dashboard() {
+  const columns = [
+    { name: "Target name", sortable: true, selector: (row) => row.Targetname },
+    { name: "Scan Type", selector: (row) => row.Scantype },
+    { name: "Last scanned", selector: (row) => row.Lastscanned },
+    { name: "Status", sortable: true, selector: (row) => row.Status },
+    { name: "Progress", sortable: true, selector: (row) => row.Progress },
+    { name: "Result", sortable: true, selector: (row) => row.Result },
+    { name: "Action", sortable: true, selector: (row) => row.Action },
+  ];
+  return (
+    <Layout>
+      <div className={styles.dashboard_container}>
+        <div className=" flex justify-between items-center">
+          <div className="flex items-center">
+            <CgProfile className=" text-7xl text-slate-100 mr-3" />
+            <div className="">
+              <h2 className="text-slate-100">Hell Ismail,</h2>
+              <h2 className="text-slate-100">Welcome Back</h2>
             </div>
           </div>
 
-          <div className="flex justify-between mt-14">
+          <div className="flex ">
+            <form action="" className="mr-8 mt-3">
+              <input
+                type="text"
+                placeholder="Example.com"
+                className="rounded-md border py-1 px-6 w-4/3 bg-transparent "
+              />
+            </form>
             <div>
-              <h2 className="text-slate-100 text-1xl"> This week (300 scans)</h2>
-            </div>
-
-            <div>
-              <h2 className="text-slate-100 text-1xl">Subdomains Details</h2>
+              <CgProfile className="text-2xl mx-auto text-slate-100" />
+              <Link className="text-slate-100" href="/">
+                {" "}
+                My profile
+              </Link>
             </div>
           </div>
         </div>
-      </Layout>
-    );
+
+        <div className="mt-14">
+          <h3 className="text-slate-100 text-3xl">Today</h3>
+          <div className="flex mt-10">
+            <div className="border border-[#3A4245] bg-[#141C1D] py-4 w-8/12 px-20 rounded-xl mr-6">
+              <h5 className="text-slate-100 text-center ">Scan Performed</h5>
+              <h1 className="text-center text-white mt-3 text-5xl">0</h1>
+            </div>
+            <div className="border border-[#3A4245] bg-[#141C1D] py-4 w-8/12 px-16  rounded-xl mr-6">
+              <h3 className="text-slate-100 text-center">Running scans</h3>
+              <h1 className="text-center text-white mt-3 text-5xl">0</h1>
+            </div>
+            <div className="border border-[#3A4245] bg-[#141C1D] py-4 w-8/12 px-16 rounded-xl mr-6">
+              <h3 className="text-slate-100 text-center">Targets</h3>
+              <h1 className="text-center text-white mt-3 text-5xl">0</h1>
+            </div>
+            <div className="border border-[#3A4245] bg-[#141C1D] py-4 w-8/12 px-16  rounded-xl p-5">
+              <h3 className="text-slate-100 text-center">Subdomains Found</h3>
+              <h1 className="text-center text-white mt-3 text-5xl">0</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-14">
+          <div>
+            <h2 className="text-slate-100 text-1xl"> This week (300 scans)</h2>
+          </div>
+
+          <div>
+            <h2 className="text-slate-100 text-1xl">Subdomains Details</h2>
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-20 items-center">
+          <h3 className="text-slate-100 text-1xl">Scan history</h3>
+
+          <div className="flex">
+            <div className="mr-5">
+              <button className="border rounded-md px-3 py-1 text-slate-100 flex ">
+                Entries{" "}
+                <span className="ml-4">
+                  <MdArrowDropDown className=" text-xl " />
+                </span>
+              </button>
+            </div>
+
+            <form action="" className=" ">
+              <input
+                type="text"
+                placeholder=" Search"
+                className="rounded-md border py-1 px-6 w-4/3 bg-transparent text-slate-100 "
+              />
+            </form>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <DataTable columns={columns} data={SCAN_HISTORY_DATA}/>
+        </div>
+      </div>
+    </Layout>
+  );
 }
