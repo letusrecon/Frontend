d87ecb41904b686f8b9b10c75164103dbb4492bc
Aytech <ayokunumitoluwani00@gmail.com>
2023-07-11 02:43:58 +0100

    Loader added

--- src/lib/Loader.js
+++ src/lib/Loader.js
@@ -1,21 +1,24 @@
-import React from "react";
-import { Oval } from "react-loader-spinner";
+
+import {ThreeCircles} from "react-loader-spinner";
 
 export default function Loader() {
   return (
     <div className="flex justify-center h-70 items-center fixed left-0 top-0 bg-black bg-opacity-50 w-[100%] h-[100vh] z-10 ">
-      <Oval
-        height={40}
-        width={40}
+     
+
+      <ThreeCircles
+        height="60"
+        width="60"
         color="#61c1d1"
         wrapperStyle={{}}
         wrapperClass=""
         visible={true}
-        ariaLabel="oval-loading"
-        secondaryColor="#fff"
-        strokeWidth={2}
-        strokeWidthSecondary={2}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor=""
+        innerCircleColor=""
+        middleCircleColor=""
       />
+   
     </div>
   );
 }
