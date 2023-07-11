c5798552e77c8fad7449a162ebf4a19014777cb3
Aytech <ayokunumitoluwani00@gmail.com>
2023-07-11 03:52:49 +0100

    Loader added

--- src/lib/Loader.js
+++ src/lib/Loader.js
@@ -3,7 +3,7 @@ import {ThreeCircles} from "react-loader-spinner";
 
 export default function Loader() {
   return (
-    <div className="flex justify-center  items-center fixed left-0 top-0 bg-black bg-opacity-50 w-[100%] h-[90vh] z-10 ">
+    <div className="flex justify-center  items-center fixed left-0 top-0 bg-black bg-opacity-50 w-[100%] h-[100vh] z-10 ">
      
 
       <ThreeCircles
